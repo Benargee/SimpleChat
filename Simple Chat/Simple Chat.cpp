d94999c69e4ab39e42d9a14f0ab1e45d07aea557
Benargee <Benargeez@gmail.com>
2015-10-07 21:46:24 -0400

    End of day build

--- Simple Chat/Simple Chat.cpp
+++ Simple Chat/Simple Chat.cpp
@@ -7,25 +7,50 @@
 
 using namespace std;
 
-int main()
+int mainChat()
 {
 	string MainPrompt;
-	cout << "Welcome to Benargee's Simple Chat.\nType ""/server"" to host a chat server\nType ""/client"" to search for a Chat server\nType ""/help"" for list of accepted commands\nAll commands are case sensitive!" << endl;
-	getline(std::cin,MainPrompt);//Puts whole line into variable with spaces http://stackoverflow.com/questions/5838711/c-cin-input-with-spaces
+	getline(cin, MainPrompt);//Puts whole line into variable with spaces http://stackoverflow.com/questions/5838711/c-cin-input-with-spaces
+	
 	if (MainPrompt == "/server")//TODO use hash with switch for commands http://stackoverflow.com/questions/16388510/evaluate-a-string-with-a-switch-in-c http://stackoverflow.com/questions/2535284/how-can-i-hash-a-string-to-an-int-using-c
 	{
-		cout <<"hosting server..." << endl;
+		cout << "hosting server..." << endl;
+		mainChat();
 	}
 	else if (MainPrompt == "/client")
 	{
 		cout << "searching for server..." << endl;
+		mainChat();
 	}
 	else if (MainPrompt == "/help")
 	{
-		cout << "Help Menu:\nType ""/server"" to host a chat server\nType ""/client"" to search for a Chat server\nType ""/help"" for list of accepted commands\nAll commands are case sensitive!" << endl;
+		cout << "Help Menu:\nType ""/server"" to host a chat server\nType ""/client"" to search for a Chat server\nType ""/help"" for list of accepted commands\nType ""/exit"" to quit Simple Chat\nAll commands are case sensitive!" << endl;
+		mainChat();
+	}
+	else if (MainPrompt == "")
+	{
+		cout << "ERROR: No text was received!" << endl;
+		mainChat();
+	}
+	else if (MainPrompt == "/exit")
+	{
+		cout << "Exiting application" << endl;
+		return 2;
+	}
+	else
+	{
+		cout << "ERROR: Invalid command!" << endl;
+		mainChat();
 	}
 	//cout  << MainPrompt[0] << endl;
-	system("pause");//Replace with better pause function!
-	return 0;
+	//system("pause");//Replace with better pause function!
+	
+}
+
+int main()
+{
+	cout << "Welcome to Benargee's Simple Chat.\nType ""/server"" to host a chat server\nType ""/client"" to search for a Chat server\nType ""/help"" for list of accepted commands\nAll commands are case sensitive!" << endl;
+	int ret = mainChat();
+	return ret;
 }
 
